323696fa955bf86312331cccd419931f0607a9a4
jose-robles2 <joserobles2230@gmail.com>
2022-11-07 20:06:25 -0800

    Added reader and writer files

--- extensions-core/pravega-indexing-service/src/main/java/org/apache/druid/Main.java
+++ extensions-core/pravega-indexing-service/src/main/java/org/apache/druid/Main.java
@@ -5,3 +5,27 @@ public class Main {
         System.out.println("Hello world!");
     }
 }
+
+/*
+* Pravega API Notes:
+*   StreamConfiguration streamConfig = StreamConfiguration.builder()
+        .scalingPolicy(ScalingPolicy.fixed(1))
+        .build();
+    * The above code defines a stream. the scaling policy referes to how many segments it'll contain (just one)
+    * The created streamConfig allows us to treat the stream like an append only file
+
+* URI controllerURI = URI.create("tcp://localhost:9090");
+    * We need this so pravega can create our stream in a certain scope (run pravega standalone in localhost)
+    * Pravega uses a stream manager to create our stream (after we've connected using the URI above)
+* try (StreamManager streamManager = StreamManager.create(controllerURI)) {
+    streamManager.createScope("tutorial");
+    streamManager.createStream("tutorial", "numbers", streamConfig);
+}
+*
+* ClientConfig clientConfig = ClientConfig.builder()
+        .controllerURI(controllerURI).build();
+* After manager creation, we need to define a clientconfig to tell the clien about the controller addresses that
+*   we conencted to pravega with the above URI
+*
+*
+* */
---
+++ extensions-core/pravega-indexing-service/src/main/java/org/apache/druid/pravegaReader.java
@@ -0,0 +1,4 @@
+package org.apache.druid;
+
+public class pravegaReader {
+}
